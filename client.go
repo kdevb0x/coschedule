2f79c5a6638816d9660a43352ee1dc49dd3699a9
Kristofer Davidson <kdevb0x@gmail.com>
2019-08-06 16:22:20 -0700

    swap names of client and server as were incorrect

--- client.go
+++ client.go
@@ -3,3 +3,21 @@
 // The full license text can be found in the LICENSE file.
 
 package coschedule
+
+type Client struct {
+	Addr          string
+	localDocState *DocumentState
+}
+
+func (d *DocumentState) AddToRenderQueue(c *Client) chan DocumentState {
+	if d.Viewers == nil {
+		d.Viewers = make(map[*Client]chan DocumentState)
+		d.Viewers[c] = make(chan DocumentState)
+	}
+	if q, ok := d.Viewers[c]; ok {
+		return q
+	}
+	cn := make(chan DocumentState)
+	d.Viewers[c] = cn
+	return cn
+}
